e4f939bebf04037b8bd2f84d8dcd3ceffb6eae25
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2023-12-26 23:56:51 +0100

    Add /mch world edit sftp

--- mch-cli/src/main/java/ca/bkaw/mch/cli/world/EditWorldCommand.java
+++ mch-cli/src/main/java/ca/bkaw/mch/cli/world/EditWorldCommand.java
@@ -4,6 +4,8 @@ import ca.bkaw.mch.repository.MchConfiguration;
 import ca.bkaw.mch.repository.MchRepository;
 import ca.bkaw.mch.repository.TrackedWorld;
 import ca.bkaw.mch.world.WorldAccessor;
+import ca.bkaw.mch.world.sftp.SftpProfile;
+import ca.bkaw.mch.world.sftp.SftpWorldAccessor;
 import ca.bkaw.mch.world.zip.ZipWorldAccessor;
 import com.google.inject.Inject;
 import picocli.CommandLine.Command;
@@ -18,6 +20,18 @@ public class EditWorldCommand {
     @Inject
     MchRepository repository;
 
+    private TrackedWorld getWorld(String identifier) {
+        MchConfiguration configuration = this.repository.getConfiguration();
+
+        for (TrackedWorld trackedWorld : configuration.getTrackedWorlds()) {
+            if (identifier.equals(trackedWorld.getName())
+                || identifier.equals(trackedWorld.getId().asHex())) {
+                return trackedWorld;
+            }
+        }
+        return null;
+    }
+
     @Command(name = "zip")
     public int zip(
         @Parameters(index = "0", paramLabel = "world name or id")
@@ -27,16 +41,7 @@ public class EditWorldCommand {
         @Parameters(index = "2", paramLabel = "path of world inside zip", defaultValue = "/")
         String pathInsideZip
     ) throws IOException {
-        MchConfiguration configuration = this.repository.getConfiguration();
-
-        TrackedWorld world = null;
-        for (TrackedWorld trackedWorld : configuration.getTrackedWorlds()) {
-            if (identifier.equals(trackedWorld.getName())
-                || identifier.equals(trackedWorld.getId().asHex())) {
-                world = trackedWorld;
-                break;
-            }
-        }
+        TrackedWorld world = this.getWorld(identifier);
         if (world == null) {
             System.err.println("World not found");
             return ExitCode.USAGE;
@@ -45,6 +50,38 @@ public class EditWorldCommand {
         zipPath = zipPath.toAbsolutePath().normalize();
         WorldAccessor worldAccessor = new ZipWorldAccessor(zipPath, pathInsideZip);
 
+        world.setWorldAccessor(worldAccessor);
+
+        this.repository.saveConfiguration();
+
+        System.out.println("Changed the world accessor.");
+        return ExitCode.OK;
+    }
+
+    @Command(name = "sftp")
+    public int sftp(
+        @Parameters(index = "0", paramLabel = "world name or id")
+        String identifier,
+        @Parameters(index = "1")
+        String sftpProfileName,
+        @Parameters(index = "2", description = "The path of the world on the remote server.")
+        String remotePath
+    ) throws IOException {
+        TrackedWorld world = this.getWorld(identifier);
+        if (world == null) {
+            System.err.println("World not found");
+            return ExitCode.USAGE;
+        }
+
+        MchConfiguration configuration = this.repository.getConfiguration();
+        SftpProfile sftpProfile = configuration.getSftpProfiles().getProfile(sftpProfileName);
+        if (sftpProfile == null) {
+            System.err.println("No SFTP profile with the name \"" + sftpProfileName + "\" exists.");
+            System.err.println("Add it using \"mch sftp add " + sftpProfileName + "\"");
+            return ExitCode.USAGE;
+        }
+
+        SftpWorldAccessor worldAccessor = new SftpWorldAccessor(sftpProfileName, remotePath);
         world.setWorldAccessor(worldAccessor);
 
         this.repository.saveConfiguration();
