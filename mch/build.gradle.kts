cba9764ef32e00a7526cd14172785ce1375b5695
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2025-12-28 17:54:30 +0100

    Add publishing to GitHub packages

--- mch/build.gradle.kts
+++ mch/build.gradle.kts
@@ -28,4 +28,20 @@ publishing {
     publications.create<MavenPublication>("maven") {
         from(components["java"])
     }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            val githubRepo = System.getenv("GITHUB_REPOSITORY") ?: ""
+            val parts = githubRepo.split("/")
+            val owner = if (parts.size >= 2) parts[0] else "OWNER"
+            val repo = if (parts.size >= 2) parts[1] else "REPO"
+            url = uri("https://maven.pkg.github.com/$owner/$repo")
+
+            credentials {
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
+                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
